b0bb5b43531f28c9171050d5614c885f6b6f77fa
Steban Ramos <juanramosiam@gmail.com>
2022-05-19 21:34:42 -0500

    splash

--- app/src/main/java/com/stebanramos/challengeintergrupo/ui/SplahActivity.java
+++ app/src/main/java/com/stebanramos/challengeintergrupo/ui/SplahActivity.java
@@ -1,20 +1,25 @@
 package com.stebanramos.challengeintergrupo.ui;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 
 import com.stebanramos.challengeintergrupo.databinding.ActivitySplahBinding;
 import com.stebanramos.challengeintergrupo.utilies.AsynResponse;
 import com.stebanramos.challengeintergrupo.utilies.GetToken;
 
-public class SplahActivity extends AppCompatActivity implements AsynResponse{
+public class SplahActivity extends AppCompatActivity implements AsynResponse {
     private static final String TAG = "SplahActivity";
-
+    boolean isReady = true;
     private ActivitySplahBinding binding;
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,7 @@ public class SplahActivity extends AppCompatActivity implements AsynResponse{
         View view = binding.getRoot();
         setContentView(view);
         Log.i(TAG, "onCreate()");
+
 
         GetToken task = new GetToken(this);
         task.delegate = (AsynResponse) this;
@@ -33,7 +39,7 @@ public class SplahActivity extends AppCompatActivity implements AsynResponse{
     public void processFinish(boolean isFinish) {
         Log.i(TAG, "onCreate() isFinish " + isFinish);
 
-        if (isFinish){
+        if (isFinish) {
             Intent intent = new Intent(SplahActivity.this, MainActivity.class);
             startActivity(intent);
         }
